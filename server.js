3dbe8ee149e1020383355cd076bdf06a6957f2a4
michal hanau <mh3182100@gmail.com>
2024-12-23 01:15:09 +0200

    delete

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+// });
 //======================================================
 
 const port = process.env.PORT || 3000;
